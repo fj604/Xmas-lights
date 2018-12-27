3e2ea604bda1cbedc28dfded1195dbba5a1edb19
fj604 <22716793+fj604@users.noreply.github.com>
2018-12-27 15:20:33 +0000

    Update xmas-lights.py

--- xmas-lights.py
+++ xmas-lights.py
@@ -13,19 +13,23 @@ WEIGHT_RED = const(5)
 WEIGHT_GREEN = const(3)
 WEIGHT_BLUE = const(3)
 
+
 def randrange(a, b):
     rnd = uos.urandom(1)[0]
     return a + rnd % (b - a)
 
+
 def rnw():
     return uos.urandom(1)[0] % (WEIGHT_RED + WEIGHT_GREEN + WEIGHT_BLUE)
-    
+
+
 machine.freq(160000000)
 np = neopixel.NeoPixel(machine.Pin(4), PIXELS)
 np.fill((0, 0, 0))
 
 while True:
-    np.buf = bytearray([c * FADE_MULTIPLIER // FADE_DIVIDER if c > 1 else 0 for c in np.buf]) 
+    np.buf = bytearray([c * FADE_MULTIPLIER // FADE_DIVIDER
+                       if c > 1 else 0 for c in np.buf])
     rnd = uos.urandom(PIXELS)
     for i in range(0, PIXELS):
         if rnd[i] < FLASH_THRESHOLD and np[i] == (0, 0, 0):
